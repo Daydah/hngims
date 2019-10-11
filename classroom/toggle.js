7e70fd927a56ad29f3f5eade4ebe5c467c2ed942
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2019-10-11 08:37:45 +0100

    Fix Classroom Page Styles

--- classroom/toggle.js
+++
@@ -1,5 +0,0 @@
-const navbarToggler = document.querySelector('.toggler');
-const sideBar = document.querySelector('.sideBar')
-navbarToggler.addEventListener('click', ()=>{
-   sideBar.classList.toggle('show-bar');
-})
